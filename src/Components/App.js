a066927aa6b17c1c2f17cdd2390ec414520f2068
Kyle Longrich <longrichk@gmail.com>
2019-08-14 07:02:16 -0400

    Hello world server with node.js getting the two talking. setting up MySQL server next

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,9 +7,53 @@ import NavBar from './headerComponents/NavBar'
 import {BrowserRouter, Route, Switch} from "react-router-dom"
 
 class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          name: '',
+          greeting: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
     
+      handleChange(event) {
+        this.setState({ name: event.target.value });
+      }
+    
+      handleSubmit(event) {
+        event.preventDefault();
+        fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+          .then(response => response.json())
+          .then(state => this.setState(state));
+      }
+
     render () {
         return (
+            <div className="App">
+        
+              <form onSubmit={this.handleSubmit}>
+                <label htmlFor="name">Enter your name: </label>
+                <input
+                  id="name"
+                  type="text"
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                />
+                <button type="submit">Submit</button>
+              </form>
+              <p>{this.state.greeting}</p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+          </div>
+
+            /*
             <BrowserRouter>
                 <NavBar />
                 <Switch>
@@ -17,6 +61,7 @@ class App extends React.Component{
                     <Route path="/growerClient" exact component={GrowerClient}/>
                 </Switch>
             </BrowserRouter>
+            */
         );
     }
 }
