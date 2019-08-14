03bb7e3e703aa1b061706adc3a9dffa6f46e86b1
Kyle Longrich <longrichk@gmail.com>
2019-08-13 23:06:58 -0400

    adding template files

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,82 +1,14 @@
 import React from 'react'
 import './App.css'
 
-import GridLayout from "react-grid-layout"
-import Header from './headerComponents/header'
-
+import GrowerClient from './pages/growerClient/growerClient'
+import UserClient from './pages/userClient/userClient'
 
 class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            productID: ''
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event){
-        this.setState({ productID: event.target.value });
-     };
-
-     handleSubmit(event) {
-        alert("Product Not Found");
-        event.preventDefault();
-     };
-
+    
     render () {
-
-        var layout = [
-            {i: 'a', x: 0, y: 0, w: 4, h: 7, static: true},
-            {i: 'b', x: 6, y: 0, w: 4, h: 3, static: true},
-            {i: 'c', x: 6, y: 3, w: 4, h: 7, static: true}
-        ];
-
         return (
-            <div class="weed-container">
-                
-                <Header />
-                
-                <div class="weed-header">
-                    <h2> Enter Product ID</h2>
-                </div>
-
-                <React.Fragment>
-                    <form onSubmit={this.handleSubmit}>
-                        <input id="inputbox"
-                            type="text"
-                            name="productID"
-                            value={this.state.id}
-                            onChange={this.handleChange}
-                        />
-                </form>
-                <h3 id="YourProduct">Your product ID: {this.state.productID}</h3>
-                </React.Fragment> 
-
-                <div class="ProductInfo">
-
-                <GridLayout class="clayout" layout={layout} cols={12} rowHeight={30} width={1000}>  
-                    <div key="a">
-                        <div class="weedPicture">
-                            Put Picture of Weed / cart here
-                        </div>
-                    </div>
-                    
-                    <div key="b"> 
-                        <div class="weedName">
-                             Put Name of Strain / Product here
-                        </div>
-                    </div>
-                    
-                    <div key="c"> 
-                        <div class="weedInfo">
-                            Put description of weed / product here
-                        </div>
-                    </div>
-                </GridLayout>
-                
-                </div>
-            </div>
+            <UserClient />
         );
     }
 }
---
+++ src/Components/pages/growerClient/growerClient.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../../headerComponents/header'
+
+class growerClient extends React.Component{
+
+    render () {
+        return (
+            <div>
+                <Header />
+
+            </div>   
+        )
+    }
+
+}
+
+export default growerClient;
