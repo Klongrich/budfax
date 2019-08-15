b0e7442d1762105fc5d5ceddaed56c8f5aebd99d
Kyle Longrich <longrichk@gmail.com>
2019-08-15 11:48:37 -0400

    Created a testing folder

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,55 +5,22 @@ import GrowerClient from './pages/growerClient/growerClient'
 import UserClient from './pages/userClient/userClient'
 import NavBar from './headerComponents/NavBar'
 import {BrowserRouter, Route, Switch} from "react-router-dom"
+import Datasubmissiontest from  '../test/dataSubmissionTest'
 
 class App extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-          name: '',
-          greeting: ''
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({ name: event.target.value });
-      }
-    
-      handleSubmit(event) {
-        event.preventDefault();
-        fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
-          .then(response => response.json())
-          .then(state => this.setState(state));
-      }
 
-    render () {
-        return (
-             <div className="App">
-        
-             <form onSubmit={this.handleSubmit}>
-                 <label htmlFor="name">Enter your name: </label>
-                <input
-                   id="name"
-                   type="text"
-                   value={this.state.name}
-                   onChange={this.handleChange}
-                 />
-                 <button type="submit">Submit</button>
-               </form>
-               <p>{this.state.greeting}</p>
-               <a
-                 className="App-link"
-                 href="https://reactjs.org"
-                 target="_blank"
-                 rel="noopener noreferrer"
-               >
-                 Learn React
-               </a>
-           </div>
+        render () {
+          return (
+            <Datasubmissiontest />
+          )
+        }
+  
+        /*
+        render () {
+          return(
+            
 
-            /*
+          
             <BrowserRouter>
                 <NavBar />
                 <Switch>
@@ -61,10 +28,11 @@ class App extends React.Component{
                     <Route path="/growerClient" exact component={GrowerClient}/>
                 </Switch>
             </BrowserRouter>
-            */
-            
-        );
-    }
+                
+
+          );
+        }   
+        */       
 }
 
 export default App;
