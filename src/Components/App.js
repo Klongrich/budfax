57199d4597a42747a1544517f10e5c7b90abdb79
Gabriel Rapaport <gabriel.rapaport@gmail.com>
2019-08-16 19:34:52 -0400

    check out my data input bitch

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,7 +7,7 @@ import NavBar from './headerComponents/NavBar'
 import {BrowserRouter, Route, Switch} from "react-router-dom"
 import Datasubmissiontest from  '../test/dataSubmissionTest'
 import growerClient from './pages/growerClient/growerClient'
-import dataEntryTest from './pages/growerClient/dataEntryTest'
+import Form from './pages/growerClient/dataEntryTest'
 
 
 
@@ -23,7 +23,7 @@ class App extends React.Component{
                     <Route path="/" exact component={UserClient}/>
                     <Route path="/growerclient" exact component={growerClient}/>
                     <Route  path="/datasubmissiontest" exact component={Datasubmissiontest} />
-                    <Route path="/dataEntryTest" exact component={dataEntryTest}/>
+                    <Route path="/dataEntryTest" exact component={Form}/>
                 </Switch>
             </BrowserRouter>              
           );
