b31fd459bed6bfb16adfb04c4f96407916247303
Kyle Longrich <longrichk@gmail.com>
2019-08-16 18:46:36 -0400

    adding sql test

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,6 +8,7 @@ import {BrowserRouter, Route, Switch} from "react-router-dom"
 import Datasubmissiontest from  '../test/dataSubmissionTest'
 import growerClient from './pages/growerClient/growerClient'
 import dataEntryTest from './pages/growerClient/dataEntryTest'
+import growerEnteryScript from '../test/growerEternyScript'
 
 
 
@@ -24,6 +25,7 @@ class App extends React.Component{
                     <Route path="/growerclient" exact component={growerClient}/>
                     <Route  path="/datasubmissiontest" exact component={Datasubmissiontest} />
                     <Route path="/dataEntryTest" exact component={dataEntryTest}/>
+                    <Route path="/growerEnteryScript" exact component={growerEnteryScript}/>
                 </Switch>
             </BrowserRouter>              
           );
