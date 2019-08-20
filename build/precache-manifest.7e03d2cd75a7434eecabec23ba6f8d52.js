7c8af5cf05c7c6cd19f2f3410845f8e3264bc8ed
Kyle Longrich <longrichk@gmail.com>
2019-08-20 00:45:19 -0400

    added files for connecting to EC2, also added a build as well as a node server app to host static build files on ec2

---
+++ build/precache-manifest.7e03d2cd75a7434eecabec23ba6f8d52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa9ce32ae641a1c497d150f0bd5742f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "920ee2aea13e57299a5d",
+    "url": "/static/css/2.b2b5939f.chunk.css"
+  },
+  {
+    "revision": "34ad68edefd6b5c398f4",
+    "url": "/static/css/main.c91e4f86.chunk.css"
+  },
+  {
+    "revision": "920ee2aea13e57299a5d",
+    "url": "/static/js/2.a33c1e72.chunk.js"
+  },
+  {
+    "revision": "34ad68edefd6b5c398f4",
+    "url": "/static/js/main.2e9548c1.chunk.js"
+  },
+  {
+    "revision": "f01334ac61633382ce30",
+    "url": "/static/js/runtime~main.34484152.js"
+  }
+]);
