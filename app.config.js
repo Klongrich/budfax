66a9564b96df8c9b8a27a529ceff6576554562b7
Kyle Longrich <longrichk@gmail.com>
2019-08-20 01:10:57 -0400

    added pm2 in order to run both node servers at the same time

---
+++ app.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    apps : [{
+      name        : "Server-API",
+      script      : "./server/index.js",
+      watch       : true,
+      env: {
+        "NODE_ENV": "development",
+      },
+      env_production : {
+       "NODE_ENV": "production"
+      }
+    },
+    {
+      name       : "Server-Client",
+      script     : "./serveStaticFiles.js",
+      watch       : true,
+      env: {
+        "NODE_ENV": "development",
+      },
+      env_production : {
+        "NODE_ENV": "production"
+      }
+    }]
+  }
