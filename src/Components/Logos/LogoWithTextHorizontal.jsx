3781c7ef898a8f69bd7681d86edd2ec0c4366b17
Maurice Christopher <maurice@opus1.io>
2019-08-23 01:10:29 -0700

    reusable horizontal text logo component

---
+++ src/Components/Logos/LogoWithTextHorizontal.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import { Logo } from "./Logo";
+
+const StyledLogoWithTextHorizontal = styled.div`
+    display: flex;
+    align-items: flex-end;
+    margin: 15px 0px 0px 15px;
+`;
+const LogoText = styled.h1`
+    color: #43a13b;
+    font-size: 30px;
+    line-height: 26px;
+    margin: 0px 0px 0px 5px;
+`;
+
+export const LogoWithTextHorizontal = ({ height, width }) => {
+    return (
+        <StyledLogoWithTextHorizontal>
+            <Logo height={height} width={width} />
+            <LogoText>BudFax</LogoText>
+        </StyledLogoWithTextHorizontal>
+    );
+};
--- src/Pages/landingPage_newDesign/index.jsx
+++ src/Pages/landingPage_newDesign/index.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import styled from "styled-components";
-import { Logo } from "../../Components/Logo";
+import { LogoWithTextHorizontal } from "../../Components/Logos/LogoWithTextHorizontal";
 
 const Body = styled.div`
-    height: 100px;
-    width: 100px;
-    background-color: green !important;
+    height: 100%;
+    width: 100%;
+    background-color: slategray;
+    top: 72px;
+    position: absolute;
+`;
+
+const Header = styled.div`
+    height: 72px;
+    width: 100%;
+    background-color: black;
+    position: fixed;
 `;
 
 const LandingPage = () => {
     return (
-        <Body>
-            <Logo height="42px" width="42px" />
-        </Body>
+        <>
+            <Header>
+                <LogoWithTextHorizontal height="42px" width="42px" />
+            </Header>
+            <Body>WEEEED</Body>
+        </>
     );
 };
 
