f806326fb1ab695c10da29ca75be8a615456fcd8
Maurice Christopher <maurice@opus1.io>
2019-08-23 02:37:08 -0700

    a very unfinished landing page

--- src/Pages/landingPage_newDesign/index.jsx
+++ src/Pages/landingPage_newDesign/index.jsx
@@ -6,15 +6,36 @@ const Body = styled.div`
     height: 100%;
     width: 100%;
     background-color: slategray;
-    top: 72px;
-    position: absolute;
+    z-index: 0;
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `;
 
 const Header = styled.div`
+    z-index: 7;
     height: 72px;
     width: 100%;
     background-color: black;
+    box-shadow: 0 0 8px black;
     position: fixed;
+`;
+
+const QrWrapper = styled.div`
+    background-color: black;
+    border-radius: 5px;
+    height: 150px;
+    width: 150px;
+    margin-top: 100px;
+`;
+
+const Input = styled.input`
+    height: 50px;
+    width: 200px;
+    margin-top: 100px;
+    background-color: white;
 `;
 
 const LandingPage = () => {
@@ -23,7 +44,10 @@ const LandingPage = () => {
             <Header>
                 <LogoWithTextHorizontal height="42px" width="42px" />
             </Header>
-            <Body>WEEEED</Body>
+            <Body>
+                <QrWrapper /> {/* Put Logic Here*/}
+                <Input />
+            </Body>
         </>
     );
 };
