30c7dcd30ad0edc52c9c6c1ef13537e9d0accfc1
Kyle Longrich <longrichk@gmail.com>
2019-08-25 01:34:32 -0400

    making a easy to update pic

---
+++ src/Components/Budpics/budpics.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+import testImage from "../../Images/BudFax_Primary_Dark.png";
+
+
+const SetImageSize = styled.img`
+    height: ${({ height }) => height};
+    width: ${({ width }) => width};
+`;
+
+export const DankBudPic = ({ width, height, file_path}) => {
+    return <SetImageSize width={width} height={height} src={require(file_path)} />;
+};
